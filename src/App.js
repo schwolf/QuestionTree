e9964be8d82ae6a8addeebcb1f2e149f496576a0
schwolf <schmidt@epsit.de>
2016-10-10 16:56:35 +0200

    fixed mutability issue

--- src/App.js
+++ src/App.js
@@ -12,9 +12,35 @@ export default class App extends Component {
   }
 
   handleChangeAntwort(frage, antwort) {
-    // hier könnte man die Angaben des Anwenders auch noch an der Service posten.
-    frage.antwort = antwort;
-    this.setState(this.state);
+    // hier könnte man die Angaben des Anwenders auch noch an den Service posten.
+
+    // do not do it like this: works, but 
+    // frage.antwort = antwort;
+    // this.setState(this.state);
+
+    const newState = Object.assign({}, this.state);
+
+    this.setAntwort(newState.fragen, frage.id, antwort);
+
+    this.setState(newState);
+
+  }
+
+  setAntwort(fragenBaum, id, antwort) {
+    for (let i = 0; i < fragenBaum.length; i++) {
+      const frage = fragenBaum[i];
+      if (frage.id === id) {
+        frage.antwort = antwort;
+      }
+      else {
+        for (let j = 0; j < frage.moeglichkeiten.length; j++) {
+          const unterfrage = frage.moeglichkeiten[j].unterfrage;
+          if (unterfrage) {
+            this.setAntwort([unterfrage], id, antwort);
+          }
+        }
+      }
+    }
   }
 
   render() {
@@ -24,9 +50,9 @@ export default class App extends Component {
           return (
             <Frage key={frage.id} frage={frage} onChangeAntwort={(f, a) => this.handleChangeAntwort(f, a) }  />
           )
-        })}
+        }) }
         <br/>
-        <button onClick={ () => { console.log(this.state) }}>Log state</button>
+        <button onClick={ () => { console.log(this.state) } }>Log state</button>
       </div>
     );
   }

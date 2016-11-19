e827f34d89a67b1f0e5b992e49a13d7220b52c0d
schwolf <schmidt@epsit.de>
2016-11-19 23:46:39 +0100

    found another arrow function with parantheses

--- src/Freitext.js
+++ src/Freitext.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Freitext = (props) =>
+const Freitext = props =>
     <input type='text' value={props.frage.antwort || ''} onChange={ev => { props.onChangeAntwort(props.frage, ev.target.value); } } />
 
 

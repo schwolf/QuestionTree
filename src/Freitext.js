df306af8d693c616ad93704ae2c84a29f10d7afc
schwolf <schmidt@epsit.de>
2016-11-26 22:00:28 +0100

    export simplified

--- src/Freitext.js
+++ src/Freitext.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Freitext = props =>
+export default Freitext = props =>
     <input type='text' value={props.frage.antwort || ''} onChange={e => { props.onChangeAntwort(props.frage, e.target.value); } } />
 
-
-export default Freitext
